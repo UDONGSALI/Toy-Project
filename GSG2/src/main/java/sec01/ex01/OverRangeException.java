a1bc9bd990dd3bf33a5ecf12f6a39a0264f9e8ca
UDONGSALI <129227923+UDONGSALI@users.noreply.github.com>
2023-06-11 21:40:26 +0900

    Add files via upload

---
+++ GSG2/src/main/java/sec01/ex01/OverRangeException.java
@@ -0,0 +1,12 @@
+package sec01.ex01;
+
+public class OverRangeException extends Exception{
+	
+	public OverRangeException() {
+		super();
+	}
+	
+	public OverRangeException(String message) {
+		super(message);
+	}
+}
